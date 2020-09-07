b26170168d3ed55cdc477a1e83e5ffa2e2306b68
abdel1413 <50152857+abdel1413@users.noreply.github.com>
2020-09-07 09:48:34 -0400

    Add files via upload
    background-color-generator

---
+++ backgrd.js
@@ -0,0 +1,38 @@
+
+var body = document.getElementById("gradient");
+
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+var h3 = document.querySelector("h3");
+
+
+function setgradient() {
+	body.style.background = "linear-gradient(to right, "+ color1.value
+	                                                    + ", "
+	                                                    + color2.value
+	                                                    +")" ;
+
+	 // h3.textContent = body.style.background + ";" ;
+
+	 h3.textContent = body.style.background + ";";
+
+}
+
+
+color1.addEventListener("input", setgradient);
+color2.addEventListener("input", setgradient);
+
+
+// function setGradient(){
+// 	body.style.background = "linear-gradient(to right,"
+// 	                          + color1.value
+// 	                          + ", "
+// 	                          + color2.value
+// 	                          + ")";
+
+// h3.textContent = body.style.background + ";";
+// }
+
+// color1.addEventListener("input", setGradient);
+// color2.addEventListener("input", setGradient);
